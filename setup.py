bdafbe23241ee9affc41c105d428e1f0c03a9d90
deruunskso92837 <deruunskso92837@users.noreply.github.com>
2026-03-01 19:08:50 +0800

    Add test for main() and declare pytest as test dependency

--- setup.py
+++ setup.py
@@ -1,2 +1,2 @@
 from setuptools import setup
-setup(name='triage-test', version='1.0.0', py_modules=['app'])
+setup(name='triage-test', version='1.0.0', py_modules=['app'], tests_require=['pytest'])
